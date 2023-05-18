7b94dbd05677c9244f61eef39d05b6520e483df0
Jason Scheffel <contact@jasonscheffel.com>
2023-05-17 21:59:24 -0400

    [chore]: create starting boilplate in metatree.py
    Import stuff, create iff name= === asdasdklajdsk, main(), setup argopt
    briefly, etc.
    
    Signed-off-by: Jason Scheffel <contact@jasonscheffel.com>

--- metatree.py
+++ metatree.py
@@ -19,6 +19,53 @@ with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import subprocess
 import time
+from typing import Any, Dict
 
 from alive_progress import alive_bar
-from argopt import _argopt
+from argopt import argopt
+
+
+def main(args: Dict[str, Any]) -> None:
+    print(args)
+
+
+def docstring() -> str:
+    return """Hello
+
+Usage:
+    metatree.py [options]
+
+Arguments:
+    -h, --help            Show this help message and exit.
+    -v, --version         Show program's version number and exit.
+
+This program requires, or optionally 'requires' other software.
+
+Such software is listed in the README.md file that accompanies this program;
+additionally, a copy of the said file can be found at:
+<https://git.sr.ht/~jason-scheffel/metatree>.
+
+SPDX-FileCopyrightText: 2023 Jason Scheffel <contact@jasonscheffel.com>
+SPDX-License-Identifier: AGPL-3.0-or-later
+
+Copyright (C) 2023 Jason Scheffel
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+if __name__ == "__main__":
+    __version__ = "0.1.0"
+    parser = argopt(docstring(), version=__version__)
+    args = parser.parse_args()
+    main(args)  # type: ignore
