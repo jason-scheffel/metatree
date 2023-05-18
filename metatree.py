2965858e6091c6c51d4d68b478223d14cdb8b470
Jason Scheffel <contact@jasonscheffel.com>
2023-05-18 15:15:04 -0400

    [feat]: add starter code for recreate_dir()
    This function will be used to recreate the dir to scrap. This function
    is not completed; however, in this implementation, it does recreate it
    with the blank files and file for each parent directory.
    
    Signed-off-by: Jason Scheffel <contact@jasonscheffel.com>

--- metatree.py
+++ metatree.py
@@ -34,15 +34,44 @@ def get_time() -> str:
     return time.strftime("%Y-%m-%d-%Z %H:%M:%S", time.localtime())
 
 
+def recreate_dir(input_path: str, output_path: str) -> None:
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    # Iterate through the input directory
+    for root, _, files in os.walk(input_path):
+        # Recreate the directory structure in the output directory
+        relative_path = os.path.relpath(root, input_path)
+        output_dir = os.path.join(output_path, relative_path)
+
+        # Not sure if this is necessary
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        # Create a .yml file with the parent directory's name
+        parent_dir_name = os.path.basename(root)
+        parent_yml_file = f"PARENT_{parent_dir_name}.yml"
+        parent_yml_path = os.path.join(output_dir, parent_yml_file)
+        open(parent_yml_path, "w").close()
+
+        # Iterate through the files in the input directory
+        for file in files:
+            # Add .yml to the end of the file names
+            # and create empty files in the output directory
+            new_file_name = f"{file}.yml"
+            new_file_path = os.path.join(output_dir, new_file_name)
+            open(new_file_path, "w").close()
+
+
 def main(args: Namespace) -> None:
-    print(args)
+    recreate_dir(args.input, args.output)
 
 
 def docstring() -> str:
     return """Hello
 
 Usage:
-    metatree.py [options]
+    metatree.py <input> <output>
 
 Arguments:
     -h, --help            Show this help message and exit.
